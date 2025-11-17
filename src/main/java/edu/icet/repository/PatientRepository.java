f7ab7533a7a0461f214506c38ff2c904368617f5
Thenujana <chenarathenujana@gmail.com>
2025-11-17 15:16:43 +0530

    created the service impl

--- src/main/java/edu/icet/repository/PatientRepository.java
+++ src/main/java/edu/icet/repository/PatientRepository.java
@@ -4,7 +4,10 @@ import edu.icet.entity.PatientEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface PatientRepository extends JpaRepository<PatientEntity,Long> {
     List<PatientEntity> findByFullName(String name);
+    Optional<PatientEntity> findByEmail(String email);
+
 }
--- src/main/java/edu/icet/service/impl/PatientServiceImpl.java
+++ src/main/java/edu/icet/service/impl/PatientServiceImpl.java
@@ -1,4 +1,67 @@
 package edu.icet.service.impl;
 
-public class PatientServiceImpl {
+import edu.icet.dto.Patient;
+import edu.icet.entity.PatientEntity;
+import edu.icet.repository.PatientRepository;
+import edu.icet.service.PatientService;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class PatientServiceImpl implements PatientService {
+    final PatientRepository patientRepository;
+    final ModelMapper modelMapper;
+    @Override
+    public void addPatient(Patient patient) {
+       patientRepository.save(modelMapper.map(patient, PatientEntity.class));
+    }
+
+    @Override
+    public List<Patient> getAll() {
+        return patientRepository.findAll()
+                .stream()
+                .map(entity -> modelMapper.map(entity, Patient.class))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public void deletePatient(Long id) {
+patientRepository.deleteById(id);
+    }
+
+    @Override
+    public void updatePatient(Patient patient) {
+patientRepository.save(modelMapper.map(patient,PatientEntity.class));
+    }
+
+    @Override
+    public Patient searchById(Long id) {
+        return modelMapper.map(patientRepository.findById(id),Patient.class);
+    }
+
+    @Override
+    public List<Patient> searchByFullName(String fullName) {
+        List<PatientEntity> byName =patientRepository.findByFullName(fullName);
+        ArrayList<Patient>patientArrayList=new ArrayList<>();
+
+        byName.forEach(PatientEntity ->{
+            patientArrayList.add(modelMapper.map(PatientEntity,Patient.class));
+        });
+        return patientArrayList;
+    }
+
+    @Override
+    public Patient searchByEmail(String email) {
+
+        PatientEntity entity = patientRepository.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("Patient not found with email: " + email));
+
+        return modelMapper.map(entity, Patient.class);
+    }
 }
